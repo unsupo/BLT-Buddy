058fe3d0922bd54bd68c5a1f36ba341a0695e915
Michael Gutin <michael.gutin@gmail.com>
2020-05-04 15:55:38 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import syncIcon from "./sync.gif";
 
-import Tabs from '@salesforce/design-system-react/components/tabs';
-import TabsPanel from '@salesforce/design-system-react/components/tabs/panel';
+
+import Button from '@salesforce/design-system-react/components/button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 
 
 class App extends Component {
-  static displayName = 'AppLauncherExample';
+  static displayName = 'BLT Buddy';
 
-	state = {
-		search: '',
-	};
+  constructor(props){
+	  super(props);
+	  this.state = {
+		on : false,
+		status : "idle",
+		working: false
+	 }
+  } 
+ 
 
 
 	componentDidMount () {
-		fetch(window.location.origin+":"+5000+'/cmd',{
+		fetch('http://'+window.location.hostname+":"+5000+'/cmd',{
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({"value": "ps -ef|grep node"})
 		})
-			.then(resp => resp.json())
+			.then(resp => resp.json())	
 			.then(data => {
 				console.log(JSON.stringify(data));
 				this.setState({data: JSON.stringify(data)})
 			});
 	}
 
-	onSearch = (event) => {
-		this.setState({ search: event.target.value });
-	};
+	changeStatus = (status) =>{
+		if(status == this.state.status){
+			return;
+		}
+		this.setState({'status' : status});	
+		console.log(this.state);
+		working = true;
+	}		
 
 	render() {
-		return (<Tabs id="tabs-example-default">
-			<TabsPanel label="Item One">{this.state.data}</TabsPanel>
-			<TabsPanel label="Item Two">Item Two Content</TabsPanel>
-			<TabsPanel label="Item Three">Item Three Content</TabsPanel>
-			<TabsPanel disabled label="Disabled">
-				Disabled Content
-			</TabsPanel>
-		</Tabs>)
+		return (
+			<div >
+				<div style ={{padding:50, 'padding-bottom': 0}}>
+					<Button variant="brand" onClick={ () => this.changeStatus('sync')} style={{height:100 , width: 200, 'font-size':50}}>Sync</Button>
+					{this.state.status == "sync" && <CircularProgress style={{height:50 , width:50, position: 'relative', top : 20, left:50}} variant="indeterminate"/>}
+				</div>
+				<div style ={{padding:50, 'padding-bottom': 0}}>
+					<Button variant="brand" onClick={ () => this.changeStatus('build')} style={{height:100 , width: 200, 'font-size':50}}>Build</Button>
+					{this.state.status == "build" && <CircularProgress style={{height:50 , width:50, position: 'relative', top : 25, left:50}} variant="indeterminate"/>}
+				</div>
+				<div style ={{padding:50, 'padding-bottom': 0}}>
+					<Button variant="brand" onClick={ () => this.changeStatus('start')} style={{height:100 , width: 200, 'font-size':50}}>Start</Button>
+					{this.state.status == "start" && <CircularProgress style={{height:50 , width:50, position: 'relative', top : 25, left:50}} variant="indeterminate"/>}
+				</div>
+				<div style ={{padding:50, 'padding-bottom': 0}}>
+					<Button variant="brand" onClick={ () => this.changeStatus('stop')} style={{height:100 , width: 200, 'font-size':50}}>Stop</Button>
+					{this.state.status == "stop" && <CircularProgress style={{height:50 , width:50, position: 'relative', top : 25, left:50}} variant="indeterminate"/>}
+				</div>
+			</div>
+		)
 	};
 }
 
