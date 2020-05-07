a332c988de5fcce5b9a5e608a8faa2efcf589407
Jonathan Arndt <jarndt@salesforce.com>
2020-05-07 14:46:52 -0600

    Thu

--- scripts/sqllite.js
+++ scripts/sqllite.js
@@ -1,5 +1,5 @@
 const {dbdir} = require("./constants");
 const path = require('path')
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database(path.join(dbdir,'bltbuddy.db'), (err)=>err?console.error(err)?'connected to sqllite db');
+const db = new sqlite3.Database(path.join(dbdir,'bltbuddy.db'), sqlite3.OPEN_CREATE, (err)=>err?console.error(err)?'connected to sqllite db');
 db.serialize()
