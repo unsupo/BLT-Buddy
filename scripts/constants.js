81d731d11b6e17ace3d50559f7945fc59d570f91
Jonathan Arndt <jarndt@salesforce.com>
2020-05-07 15:51:31 -0600

    Thu

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,19 +2,17 @@ const {resolveHome} = require('./cmd')
 const path = require('path')
 
 const logdir = resolveHome(path.join("~","logs"))
-const basedir = 'data'
+const basedir = path.join(__dirname,'data')
 const cmddir = path.join(basedir,"cmd")
 const cmdlogdir = path.join(cmddir,"logs")
 const piddir = path.join(cmddir,'pids')
 const scriptsdir = path.join(cmddir,'scripts')
 const logfile = path.join(logdir,"blt-buddy.log")
 
-module.exports = Object.freeze({
-    logdir: logdir,
-    basedir: basedir,
-    cmddir: cmddir,
-    cmdlogdir: cmdlogdir,
-    piddir: piddir,
-    scriptsdir: scriptsdir,
-    logfile: logfile
-})
+exports.logdir = logdir
+exports.basedir = basedir
+exports.cmddir = cmddir
+exports.cmdlogdir = cmdlogdir
+exports.piddir = piddir
+exports.scriptsdir = scriptsdir
+exports.logfile = logfile
