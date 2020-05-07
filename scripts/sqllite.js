3b1acd74b19e3c477574e59d60b2cbab45dcedaf
Jonathan Arndt <jarndt@salesforce.com>
2020-05-07 14:54:26 -0600

    Thu

--- scripts/sqllite.js
+++ scripts/sqllite.js
@@ -1,5 +1,16 @@
-const {dbdir} = require("./constants");
-const path = require('path')
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database(path.join(dbdir,'bltbuddy.db'), sqlite3.OPEN_CREATE, (err)=>err?console.error(err)?'connected to sqllite db');
-db.serialize()
+const sqlite3 = require('sqlite3')
+const Promise = require('bluebird')
+
+class AppDAO {
+    constructor(dbFilePath) {
+        this.db = new sqlite3.Database(dbFilePath, (err) => {
+            if (err) {
+                console.log('Could not connect to database', err)
+            } else {
+                console.log('Connected to database')
+            }
+        })
+    }
+}
+
+module.exports = AppDAO
