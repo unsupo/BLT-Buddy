e6504765bae94eebff42b1a61e60cfb968d16c8b
Jonathan Arndt <jarndt@salesforce.com>
2020-05-07 14:46:22 -0600

    Thu

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,8 +2,10 @@ const cmd = require('./cmd')
 const path = require('path')
 
 const logdir = cmd.resolveHome(path.join("~","logs"))
+const basedir = 'data'
 
-exports.piddir = 'pids'
+exports.dbdir = path.join(basedir,'db')
+exports.piddir = path.join(basedir,'pids')
 exports.logdir = logdir
 
 exports.logfile = path.join(logdir,"blt-buddy.log")
--- scripts/sqllite.js
+++ scripts/sqllite.js
@@ -1,3 +1,5 @@
+const {dbdir} = require("./constants");
+const path = require('path')
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database();
+const db = new sqlite3.Database(path.join(dbdir,'bltbuddy.db'), (err)=>err?console.error(err)?'connected to sqllite db');
 db.serialize()
