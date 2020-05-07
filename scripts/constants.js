b6c6b056b65c4f8d097eebec2f63e080897b9af5
Jonathan Arndt <jarndt@salesforce.com>
2020-05-07 15:45:32 -0600

    Thu

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,9 +5,9 @@ const path = require('path')
 module.exports = Object.freeze({
     logdir: resolveHome(path.join("~","logs")),
     basedir: 'data',
-    cmddir: path.join(this.basedir,"cmd"),
-    cmdlogdir: path.join(this.cmddir,"logs"),
-    piddir: path.join(this.cmddir,'pids'),
-    scriptsdir: path.join(this.cmddir,'scripts'),
-    logfile: path.join(this.logdir,"blt-buddy.log")
+    cmddir: path.join(basedir,"cmd"),
+    cmdlogdir: path.join(cmddir,"logs"),
+    piddir: path.join(cmddir,'pids'),
+    scriptsdir: path.join(cmddir,'scripts'),
+    logfile: path.join(logdir,"blt-buddy.log")
 })
