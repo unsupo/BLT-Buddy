27e37c2c32907e55b278af46b6a8d5be708687ff
Jonathan Arndt <jarndt@salesforce.com>
2020-05-08 15:04:25 -0600

    Fri May  8 21:04:24 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/scripts/blt.js|PlatformSpecific Updated IsFile

--- scripts/blt.js
+++ scripts/blt.js
@@ -27,6 +27,7 @@ const commands = {
     disable_blt: `${blt} ${replace_project} --enable`,
     start_blt: `${blt} ${replace_project} --start-bg`,
     get_project_dirs: `find ${blt_app_dir} -type d -maxdepth 1`,
+    get_project_dir_status: `${working_dir}`
     restart_blt: `timeout 10 ${blt} ${replace_project} || ps -ef|grep bl[t] |awk '{print $2}'|xargs kill -9
 ${blt} ${replace_project} --db-stop
 ${blt} ${replace_project} --db-start
@@ -46,6 +47,7 @@ exports.enable_blt = () => run_cmd(commands.enable_blt)
 exports.disable_blt = () => run_cmd(commands.disable_blt)
 exports.start_blt = () => run_cmd(commands.start_blt)
 exports.get_project_dirs = () => run_cmd(commands.get_project_dirs)
+exports.get_project_dir_status = () => run_cmd(commands.get_project_dir_status)
 
 exports.set_project = (dir) => {
     working_dir = resolveHome(path.join("~", "blt", dir));
