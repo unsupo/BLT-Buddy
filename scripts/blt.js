cfcad2adb4356b471561bee6cc63ca020b754154
Jonathan Arndt <jarndt@salesforce.com>
2020-05-08 20:46:20 -0600

    Sat May  9 02:46:20 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/scripts/blt.js|PlatformSpecific Updated IsFile

--- scripts/blt.js
+++ scripts/blt.js
@@ -59,7 +59,7 @@ exports.set_project = (dir) => new Promise(resolve => {
     project = proj+projectDir
     working_dir_cmd = "cd " + working_dir + " && ";
     // TODO save working dir to disk and pick it up on startup
-    fs.writeFileSync(projectFile,dir)
+    fs.writeFileSync(constants.projectFile,dir)
     console.log(working_dir+" "+project)
     resolve(working_dir)
 })
