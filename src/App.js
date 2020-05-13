d842015ab5fb27b6b275218222ace1b059bc699c
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 12:47:35 -0600

    Wed May 13 18:47:35 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/App.js|PlatformSpecific Updated IsFile

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
                     size="large"
                     backgroundSize="cover"
                     className="slds-p-around_large"
-                    theme="default">
+                    theme="lightning-blue">
                     <div className="slds-box slds-theme_default">
                         <h3 className="slds-text-heading_label slds-truncate">Docs</h3>
                     </div>
