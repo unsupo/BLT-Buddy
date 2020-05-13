54121092506448c9f8eecff5f2650cf1e3ee1f17
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 14:24:08 -0600

    Wed May 13 20:24:08 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/docs/Docs.js|PlatformSpecific Updated IsFile

--- src/components/docs/Docs.js
+++ src/components/docs/Docs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BrandBand from '@salesforce/design-system-react/components/brand-band';
-var __html = require('./template.html');
+var __html = require('/Users/jarndt/blt/blt-code/plugins/blt/doc/cheat/cheat.html');
 
 class Docs extends React.Component {
     render() {
