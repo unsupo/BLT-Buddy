38065d33e54bcc0e14765eeafcd708b1edd1e3c3
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 11:23:48 -0600

    Wed May 13 17:23:48 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/main.js|PlatformSpecific Updated IsFile

--- main.js
+++ main.js
@@ -82,7 +82,8 @@ function createWindow() {
     // win.loadFile(__dirname + "/build/index.html");
     win.loadURL('http://localhost:3000')
     win.on('close',(event)=>{
-        if(!app.state.is)
+        event.preventDefault()
+        
     })
     return win
 }
