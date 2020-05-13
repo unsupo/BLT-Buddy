726ea54231d6ff2a13bce2635a0f9c336b394214
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 13:31:11 -0600

    Wed May 13 19:31:11 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/App.js|PlatformSpecific Updated IsFile

--- src/App.js
+++ src/App.js
@@ -34,7 +34,11 @@ class App extends Component {
     render() {
         return (
             <IconSettings iconPath="/assets/icons">
-                <MyAppLauncher apps={[{'title': 'Docs', 'iconText': 'DO', 'description': 'Documentation'}]}/>
+                <MyAppLauncher apps={
+                    [
+                        {'title': 'Documentation', 'iconText': 'DO', 'description': 'Documentation'},
+                        {'title': 'Configs', 'iconText': 'CO', 'description': 'Configurations'}
+                    ]}/>
             </IconSettings>
         )
     };
