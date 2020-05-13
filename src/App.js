5645ee0640907dc13cedb866e28b05dec08aed4e
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 12:53:12 -0600

    Wed May 13 18:53:11 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/App.js|PlatformSpecific Updated IsFile

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
                 <BrandBand
                     id="brand-band-lightning-blue"
                     size="large"
-                    ba
+                    image="none"
                     backgroundSize="cover"
                     className="slds-p-around_large">
                     <div className="slds-box slds-theme_default">
