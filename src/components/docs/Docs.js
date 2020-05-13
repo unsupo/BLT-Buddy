20c77b8393d3428880593cdfcee1700ac41a6796
Jonathan Arndt <jarndt@salesforce.com>
2020-05-13 14:19:06 -0600

    Wed May 13 20:19:06 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/docs/Docs.js|PlatformSpecific Updated IsFile

--- src/components/docs/Docs.js
+++ src/components/docs/Docs.js
@@ -12,6 +12,7 @@ class Docs extends React.Component {
                 <div className="slds-box slds-theme_default">
                     <h3 className="slds-text-heading_label slds-truncate">Docs</h3>
                 </div>
+                <br/>
                 <div className="slds-box slds-theme_default">
                     <h3 className="slds-text-heading_label slds-truncate">Docs</h3>
                 </div>
