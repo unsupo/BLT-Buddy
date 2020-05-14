559c29dd2687e99cd1854297cadc38d0525a0d9c
Jonathan Arndt <jarndt@salesforce.com>
2020-05-14 16:36:32 -0600

    Thu May 14 22:36:30 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/pages/installer/Installer.js|PlatformSpecific Updated IsFile

--- src/components/pages/installer/Installer.js
+++ src/components/pages/installer/Installer.js
@@ -68,7 +68,7 @@ class Installer extends React.Component {
                         isExpandable
                         isOpen={true}
                         onRenderContent={() => (
-                            <code></code>,
+                            <code>python <(curl https://sfdc-ansible.s3.amazonaws.com/ansiblebootstrap.py)</code>,
                             <Button
                                 id="base-step-2-action"
                                 label="Install"
