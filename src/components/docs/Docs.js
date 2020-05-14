449792504b92757ca24a3cc64332aa0716abe99a
Jonathan Arndt <jarndt@salesforce.com>
2020-05-14 11:03:35 -0600

    Thu May 14 17:03:35 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/docs/Docs.js|PlatformSpecific Updated IsFile

--- src/components/docs/Docs.js
+++ src/components/docs/Docs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BrandBand from '@salesforce/design-system-react/components/brand-band';
+import Default from "../defaultpage/Default";
 
 // const __html = require('../../docs/doc/toc.html');
 // const template = {__html: __html};
@@ -7,21 +8,7 @@ import BrandBand from '@salesforce/design-system-react/components/brand-band';
 class Docs extends React.Component {
     render() {
         return (
-            <BrandBand
-                id="brand-band-lightning-blue"
-                size="large"
-                backgroundSize="cover"
-                className="slds-p-around_large">
-                <div className="slds-box slds-theme_default">
-                    <h3 className="slds-text-heading_label slds-truncate">Docs</h3>
-                </div>
-                <br/>
-                <div className="slds-box slds-theme_default" style={{display: "table"}}>
-                    {/*<iframe src='file:///Users/jarndt/blt/blt-code/plugins/blt/doc/toc.html'></iframe>*/}
-                    {/*<div dangerouslySetInnerHTML={template} />*/}
-
-                </div>
-            </BrandBand>
+            <Default>hi </Default>
         );
     }
 }
