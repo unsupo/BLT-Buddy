7f4340f2e5b77223bd4bebd44d6685254c158dcc
Jonathan Arndt <jarndt@salesforce.com>
2020-05-14 16:09:00 -0600

    Thu May 14 22:09:00 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/App.js|PlatformSpecific Updated IsFile

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
     };
     displayAlert() {
         if(this.state.alert)
-            return (<AlertContainer>
+            return (<AlertContainer className='slds-accordion'>
                 <Alert
                     icon={<Icon category="utility" name="error" />}
                     labels={{
