31aa9d45692ab960381ea7707c5c2f1fd77747c4
Jonathan Arndt <jarndt@salesforce.com>
2020-05-14 11:17:02 -0600

    Thu May 14 17:16:31 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/timings/Timings.js|Created IsFile

---
+++ src/components/actions/Actions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Default from "../defaultpage/Default";
+
+/*
+This is where you create actions that you can use in the tray
+or you can use in Timings
+ */
+class Actions extends React.Component {
+    render() {
+        return (
+            <Default title={'Configurations'}>
+                <p>HI</p>
+            </Default>
+        );
+    }
+}
+export default Actions;
---
+++ src/components/timings/Timings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Default from "../defaultpage/Default";
+
+/*
+This is where you create timings.  Timings are used
+to schedule when actions take place.
+
+ */
+class Timings extends React.Component {
+    render() {
+        return (
+            <Default title={'Configurations'}>
+                <p>HI</p>
+            </Default>
+        );
+    }
+}
+export default Timings;
