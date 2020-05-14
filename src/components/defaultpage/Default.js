a083c8af3e9c23e49be0b647bccd0e85e748e58f
Jonathan Arndt <jarndt@salesforce.com>
2020-05-14 11:02:55 -0600

    Thu May 14 17:01:54 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/defaultpage/Default.js|Created IsFile

---
+++ src/components/defaultpage/Default.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BrandBand from '@salesforce/design-system-react/components/brand-band';
+
+class Default extends React.Component {
+    render() {
+        return (
+            <BrandBand
+                id="brand-band-lightning-blue"
+                size="large"
+                backgroundSize="cover"
+                className="slds-p-around_large">
+                <div className="slds-box slds-theme_default">
+                    <h3 className="slds-text-heading_label slds-truncate">{this.props.title}</h3>
+                </div>
+                <br/>
+                <div className="slds-box slds-theme_default">
+
+                </div>
+            </BrandBand>
+        );
+    }
+}
+export default Default;
