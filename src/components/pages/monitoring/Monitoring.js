5ff03c6440711f840ec95dcbd1527e3b73b50fcd
Jonathan Arndt <jarndt@salesforce.com>
2020-05-15 15:44:23 -0600

    Fri May 15 21:44:21 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/src/components/pages/monitoring/Monitoring.js|PlatformSpecific Updated IsFile

--- src/components/pages/monitoring/Monitoring.js
+++ src/components/pages/monitoring/Monitoring.js
@@ -31,7 +31,7 @@ class Monitoring extends React.Component {
         clearInterval(this.interval);
     }
     render() {
-        this.get_app_health()
+        this.interval = setInterval(() => this.get_app_health, 1000);
         return (
             <Default title={'Monitoring'}>
                 <PageHeader
