071150cc9bcb5b10e8e37ca38bac8301bde2f8e2
Jonathan Arndt <jarndt@salesforce.com>
2020-05-18 14:18:08 -0600

    Initial commit

--- src/components/pages/actions/Actions.js
+++ src/components/pages/actions/Actions.js
@@ -1,16 +1,185 @@
 import React from 'react';
 import Default from "../../defaultpage/Default";
+import SplitView from "@salesforce/design-system-react/components/split-view";
+import SplitViewHeader from "@salesforce/design-system-react/components/split-view/header";
+import Icon from "@salesforce/design-system-react/components/icon";
+import SplitViewListbox from "@salesforce/design-system-react/components/split-view/listbox";
 
 /*
 This is where you create actions that you can use in the tray
 or you can use in Timings
  */
+
+const SORT_OPTIONS = {
+    UP: 'up',
+    DOWN: 'down',
+};
+const listOptions = [
+    {
+        id: '001',
+        label: 'Riley Shultz',
+        topRightText: '99',
+        bottomLeftText: 'Biotech, Inc.',
+        bottomRightText: 'Nurturing',
+    },
+    {
+        id: '002',
+        label: 'Jason A. - VP of Sales',
+        topRightText: '92',
+        bottomLeftText: 'Case Management Solutions',
+        bottomRightText: 'Contacted',
+    },
+]
 class Actions extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: typeof props.isOpen === 'boolean' ? props.isOpen : true,
+            options: listOptions,
+            selected: [listOptions[listOptions.length - 2]],
+            unread: [listOptions[0], listOptions[2]],
+            sortDirection: SORT_OPTIONS.DOWN,
+        };
+
+        this.sortList = this.sortList.bind(this);
+    }
+    sortList() {
+        const sortDirection =
+            this.state.sortDirection === SORT_OPTIONS.DOWN
+                ? SORT_OPTIONS.UP
+                : SORT_OPTIONS.DOWN;
+
+        this.setState({
+            options: this.state.options.sort(
+                (a, b) =>
+                    sortDirection === SORT_OPTIONS.DOWN
+                        ? a.label > b.label
+                        : b.label > a.label
+            ),
+            sortDirection,
+        });
+    }
+
     render() {
         return (
             <Default title={'Actions'}>
-                <p>HI</p>
+                <div style={{ height: '90vh' }}>
+                    <SplitView
+                        events={{
+                            onClose: () => {
+                                this.setState({ isOpen: false });
+                            },
+                            onOpen: () => {
+                                this.setState({ isOpen: true });
+                            },
+                        }}
+                        id="base-example"
+                        isOpen={this.state.isOpen}
+                        master={this.masterView()}
+                        detail={this.detailView()}
+                    />
+                </div>
             </Default>
+        );
+    }
+    masterView() {
+        return [
+            <SplitViewHeader
+                key="1"
+                // onRenderActions={headerActions}
+                // onRenderControls={headerControls}
+                icon={
+                    <Icon
+                        assistiveText={{ label: 'User' }}
+                        category="standard"
+                        name="action_list_component"
+                    />
+                }
+                info="Create Actions"
+                // info="42 items • Updated just now"
+                title={"Actions"}
+                truncate
+                variant="object-home"
+            />,
+            <SplitViewListbox
+                key="2"
+                labels={{
+                    header: 'Lead Score',
+                }}
+                sortDirection={this.state.sortDirection}
+                options={this.state.options}
+                events={{
+                    onSort: this.sortList,
+                    onSelect: (event, { selectedItems, item }) => {
+                        this.setState({
+                            unread: this.state.unread.filter((i) => i !== item),
+                            selected: selectedItems,
+                        });
+                    },
+                }}
+                selection={this.state.selected}
+                unread={this.state.unread}
+            />,
+        ];
+    }
+
+    detailView() {
+        return this.state.selected.length ? (
+            this.state.selected.map((item) => (
+                <dl
+                    key={item.id}
+                    className="slds-box slds-m-left_medium slds-m-bottom_medium slds-list_horizontal slds-wrap"
+                >
+                    <dt
+                        className="slds-item_label slds-text-color_weak slds-truncate"
+                        title="Name"
+                    >
+                        Name:
+                    </dt>
+                    <dd className="slds-item_detail slds-truncate" title={item.label}>
+                        {item.label}
+                    </dd>
+                    <dt
+                        className="slds-item_label slds-text-color_weak slds-truncate"
+                        title="Value"
+                    >
+                        Value:
+                    </dt>
+                    <dd
+                        className="slds-item_detail slds-truncate"
+                        title={item.topRightText}
+                    >
+                        {item.topRightText}
+                    </dd>
+                    <dt
+                        className="slds-item_label slds-text-color_weak slds-truncate"
+                        title="Company"
+                    >
+                        Company:
+                    </dt>
+                    <dd
+                        className="slds-item_detail slds-truncate"
+                        title={item.bottomLeftText}
+                    >
+                        {item.bottomLeftText}
+                    </dd>
+                    <dt
+                        className="slds-item_label slds-text-color_weak slds-truncate"
+                        title="Status"
+                    >
+                        Status:
+                    </dt>
+                    <dd
+                        className="slds-item_detail slds-truncate"
+                        title={item.bottomRightText}
+                    >
+                        {item.bottomRightText}
+                    </dd>
+                </dl>
+            ))
+        ) : (
+            <div />
         );
     }
 }
--- src/components/pages/monitoring/Monitoring.js
+++ src/components/pages/monitoring/Monitoring.js
@@ -4,15 +4,28 @@ import {PageHeaderControl} from "@salesforce/design-system-react";
 import PageHeader from "@salesforce/design-system-react/components/page-header";
 import Icon from "@salesforce/design-system-react/components/icon";
 import isElectron from "is-electron";
+import DataTable from "@salesforce/design-system-react/components/data-table";
+import DataTableColumn from "@salesforce/design-system-react/components/data-table/column";
+import DataTableCell from "@salesforce/design-system-react/components/data-table/cell";
 
 /*
 This is where you create actions that you can use in the tray
 or you can use in Timings
  */
+
+const IconDataTableCell = ({ children, ...props }) => (
+    <DataTableCell title={children} {...props}>
+        <Icon
+            assistiveText={{ label: 'User' }}
+            category="standard"
+            name={console.log(children,props) ? "task2" : ""}
+        />
+    </DataTableCell>
+);
+
 class Monitoring extends React.Component {
     constructor(props) {
         super(props);
-        this.interval = setInterval(() => this.get_app_health, 1000);
     }
     state = {
         health: '',
@@ -20,28 +33,26 @@ class Monitoring extends React.Component {
     get_app_health(){
         if(isElectron()) {
             window.ipcRenderer.invoke('api', {'cmd': 'check-health'}).then(value => {
-                this.setState({health: JSON.parse(value['res'])})
+                const res = JSON.parse(value['res'])
+                this.setState({health: res})
+                const items = []
+                for (let [key, value] of Object.entries(res['app']))
+                    items.push({name: key, status: value})
+
+                this.setState({items: items})
             })
         }
     }
     componentDidMount() {
-        this.interval = setInterval(() => this.get_app_health, 1000);
+        this.interval = setInterval(() => this.get_app_health(), 1000);
     }
     componentWillUnmount() {
         clearInterval(this.interval);
     }
     render() {
-        this.interval = setInterval(() => this.get_app_health(), 1000);
         return (
             <Default title={'Monitoring'}>
                 <PageHeader
-                    details={[
-                        {
-                            label: 'Field 1',
-                            content:
-                                'Description that demonstrates truncation with content. Description that demonstrates truncation with content.',
-                            truncate: true}
-                    ]}
                     icon={
                         <Icon
                             assistiveText={{ label: 'User' }}
@@ -49,17 +60,30 @@ class Monitoring extends React.Component {
                             name={this.isRunning() ? "task2" : "first_non_empty"}
                         />
                     }
-                    label="Status"
+                    label="App Status"
                     // onRenderActions={actions}
                     title={this.isRunning() ? "Running" : "Stopped"}
                     variant="record-home"
                 />
+                <DataTable items={this.state.items}
+                           id="DataTableExample-1-default"
+                           fixedHeader
+                           fixedLayout
+                           joined>
+                    <DataTableColumn key="name" label="Name" property="name" />
+                    <DataTableColumn key="status" label="Status" property="status" >
+                        <IconDataTableCell />
+                    </DataTableColumn>
+                </DataTable>
             </Default>
         );
     }
 
     isRunning() {
-        return this.state.hasOwnProperty('health');
+        return this.state.hasOwnProperty('health')
+            && this.state.health.hasOwnProperty('app')
+            && this.state.health['app'].hasOwnProperty('ui_check')
+            && this.state.health['app']['ui_check'] === "UP";
 
         // return this.state.health && this.state.health['app'] && this.state.health['app']['ui_check'] === "UP";
     }
