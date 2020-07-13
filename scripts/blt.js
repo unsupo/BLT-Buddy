d6ea5d6ca12e36b23def09534fc7a7b66d0d994c
Jonathan Arndt <jarndt@salesforce.com>
2020-07-13 13:25:45 -0600

    Mon Jul 13 19:25:43 2020|/Users/jarndt/code_projects/TMP-Spring-20-Hackathon/data/cmd/timings/ae323b9d974336c3016ca2df1754582e.timings|Updated IsFile

--- scripts/blt.js
+++ scripts/blt.js
@@ -54,10 +54,10 @@ const run_cmd = (cmd_key) => command(cmd_replacer(cmd_key))
 const _cmd = (cmd_key) => cmd(cmd_replacer(cmd_key))
 
 exports.getCmdKey = (hash) => {
-    constants.fo {
+    Object.keys(constants).forEach(v => {
         if(md5(cmd_replacer(v))===hash)
-            return k;
-    }
+            return v;
+    });
     return null;
 }
 
